5064b7db98ff4a9359ecdf04743dbb4c77b8dd52
José Manuel Barroso Galindo <theypsilon@gmail.com>
2022-01-17 19:13:45 +0100

    Catching failing childs

--- .github/update_distribution.sh
+++ .github/update_distribution.sh
@@ -22,7 +22,15 @@ update_distribution() {
         done
     done
 
-    wait
+    FAILING_CHILD=
+    for job in `jobs -p` ; do
+        wait ${job} || let "FAILING_CHILD=${job}"
+    done
+
+    if [[ "${FAILING_CHILD}" != "" ]] ; then
+        echo "Child failed: ${FAILING_CHILD}"
+        exit 1
+    fi
 
     if [[ "${PUSH_COMMAND}" != "--push" ]] ; then
         return

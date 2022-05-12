50f33dcea67133747446ca1df73f19e8deba3dc7
José Manuel Barroso Galindo <theypsilon@gmail.com>
2022-05-13 00:26:02 +0200

    Fixing zips branch

--- .github/calculate_db.py
+++ .github/calculate_db.py
@@ -461,9 +461,7 @@ def create_db(folder, options, tags):
             if not run_conditional('git diff --quiet main origin/zips'):
                 print('zip branch has changes')
                 run_succesfully('git update-ref -d refs/remotes/origin/zips')
-                run_succesfully('git push --force origin new_zips:zips2')
-                print('forcing error')
-                raise Error('boom')
+                run_succesfully('git push --force origin new_zips:zips')
             else:
                 print('Using old zip branch from origin')
                 zip_sha = run_stdout('git rev-parse --verify origin/zip').strip()

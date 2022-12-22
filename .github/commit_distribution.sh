0cdef1d4c995cc1d8240a8730fb4c60e0e39be11
José Manuel Barroso Galindo <theypsilon@gmail.com>
2022-12-22 12:04:30 +0100

    No need to run detox anymore here.

--- .github/commit_distribution.sh
+++ .github/commit_distribution.sh
@@ -6,16 +6,6 @@ set -euo pipefail
 git config --global user.email "theypsilon@gmail.com"
 git config --global user.name "The CI/CD Bot"
 git checkout -f develop -b main
-echo "Running detox"
-detox -v -s utf_8-only -r *
-echo "Detox done"
-echo "Removing colons"
-IFS=$'\n'
-for i in $(find . -name "*:*"); do
-    echo mv "${i}" "${i/:/-}"
-    mv "${i}" "${i/:/-}"
-done
-echo "Colons removed"
 git add .
 git commit -m "-"
 git fetch origin main || true

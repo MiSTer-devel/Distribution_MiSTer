2ee358b54388b764bf112b767e6168f0d7fd0a74
José Manuel Barroso Galindo <theypsilon@gmail.com>
2023-09-11 18:35:52 +0200

    Debugging error

--- .github/download_distribution.py
+++ .github/download_distribution.py
@@ -789,6 +789,7 @@ def download_file(url: str, target: str) -> None:
 # execution utilities
 
 def run(command: str, cwd: Optional[str] = None) -> None:
+    print(command)
     _run(command, cwd, stderr=subprocess.STDOUT, stdout=None)
 
 def run_stdout(command: str, cwd: Optional[str] = None) -> str:

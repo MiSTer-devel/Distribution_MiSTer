dbe6517f3f35e4c022b9452ce9d14d27bb298a24
José Manuel Barroso Galindo <theypsilon@gmail.com>
2024-03-03 18:39:54 +0100

    Stop distributing downloader_latest.sh, but keep .zip

--- .github/download_distribution.py
+++ .github/download_distribution.py
@@ -201,7 +201,6 @@ def fetch_extra_content_urls() -> List[str]:
     result.extend([("/Scripts/", "https://raw.githubusercontent.com/MiSTer-devel/Scripts_MiSTer/master/other_authors/wifi.sh")])
     result.extend([("/Scripts/", "https://raw.githubusercontent.com/MiSTer-devel/Scripts_MiSTer/master/rtc.sh")])
     result.extend([("/Scripts/", "https://raw.githubusercontent.com/MiSTer-devel/Scripts_MiSTer/master/timezone.sh")])
-    result.extend([("/Scripts/.config/downloader/downloader_latest.sh", "https://raw.githubusercontent.com/MiSTer-devel/Downloader_MiSTer/main/dont_download.sh")])
     result.extend([("/Scripts/.config/downloader/downloader_latest.zip", "https://github.com/MiSTer-devel/Downloader_MiSTer/releases/download/latest/dont_download.zip")])
     result.extend(["user-content-unzip"])
     result.extend([("/games/VECTREX/Overlays/", "https://raw.githubusercontent.com/MiSTer-devel/Vectrex_MiSTer/master/overlays/overlays.zip")])

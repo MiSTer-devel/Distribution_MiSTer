9d64311c143d6a5187d220e38fdc9e0359e60724
José Manuel Barroso Galindo <theypsilon@gmail.com>
2025-12-29 00:32:25 +0100

    Refactor test_database.sh to use downloader_test.py
    Removed old downloader setup and replaced with a test script.

--- .github/test_database.sh
+++ .github/test_database.sh
@@ -3,38 +3,14 @@
 
 set -euo pipefail
 
+echo "Testing ${DB_ID}: ${DB_JSON_NAME}"
+echo
+
 DB_URL="$(pwd)/${DB_JSON_NAME}"
-DOWNLOADER="$(pwd)/Scripts/.config/downloader/downloader_latest.zip"
-
 cd "$(mktemp -d)"
-echo "[mister]" > downloader.ini
-echo "base_path = $(pwd)" >> downloader.ini
-echo "base_system_path = $(pwd)" >> downloader.ini
-echo "update_linux = false" >> downloader.ini
-echo "allow_reboot  = 0" >> downloader.ini
-echo "verbose = false" >> downloader.ini
-echo "downloader_retries = 0" >> downloader.ini
-echo "[${DB_ID}]" >> downloader.ini
-echo "db_url = ${DB_URL}" >> downloader.ini
-
-echo "downloader.ini :"
-cat downloader.ini
-echo
-if [ -f "${DOWNLOADER}" ] ; then
-  cp "${DOWNLOADER}" downloader
-else
-  # This allows this script to be used as a dependency. It's playing that role for other builds already, so please keep it in place.
-  curl --show-error --fail --location -o "downloader" "https://github.com/MiSTer-devel/Downloader_MiSTer/releases/download/latest/dont_download.zip"
-fi
-chmod +x downloader
-
-echo
-echo "Running downloader"
-export SKIP_FREE_SPACE_CHECKS=true
-export DEBUG=true
-export CURL_SSL=""
-export DOWNLOADER_INI_PATH="$(pwd)/downloader.ini"
-./downloader
+curl --show-error --fail --location -o "downloader_test.py" "https://github.com/MiSTer-devel/Downloader_MiSTer/releases/download/latest/downloader_test.py"
+chmod +x downloader_test.py
+./downloader_test.py "${DB_ID}" "${DB_URL}"
 
 echo
 echo "The test went well."
